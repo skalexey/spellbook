710cb6e9d5ef145ad1bbda3c0da763c1643a6fef
Alex <skorohodov-ad@narod.ru>
2022-05-15 21:54:22 +0300

    Added build_utils.py

---
+++ build_utils.py
@@ -0,0 +1,46 @@
+import shutil
+import os
+import sys
+
+buildPrefix = "Build-cmake"
+buildConfig = "Debug"
+
+def systemRemoveDir(path):
+    code = -1
+    if os.name == 'nt':
+        code = os.system("rm \"" + path + "\"")
+    else:
+        code = os.system("rm -rf \"" + path + "\"")
+    if code == 0:
+        print("Directory '%s' removed" % path)
+    else:
+        print("Return code: %d" % code)
+
+def removeDir(path):
+    if os.path.exists(path):
+        if not os.access(path, os.W_OK):
+            os.chmod(path, stat.S_IWUSR)
+        try:
+            shutil.rmtree(path)
+            print("Directory '%s' removed" % path)
+        except OSError as e:
+            print("Exception while removing a directory '%s': %s: '%s'" % (path, e.strerror, e.filename))
+            print("Try to use shell command")
+            systemRemoveDir(path)
+
+    else:
+        print("'%s' directory already removed" % path)
+
+for a in sys.argv:
+    if a == "release":
+        buildConfig = "Release"
+    if a == "g++":
+        buildPrefix="Build-g++"
+    if a == "full":
+    	   buildConfig = ""
+
+buildFolder = buildPrefix
+buildConfigFolder = buildPrefix + "/" + buildConfig
+
+
+
