175f1a6b5e47756c96168eabddcff5f3d666a0df
skalexey <skorohodov-ad@narod.ru>
2022-07-20 17:45:36 +0300

    Add list_spell.

---
+++ Spellbook/include/spells/list_spell.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <unordered_map>
+#include "spell.h"
+#include "spell_factory.h"
+
+namespace spl
+{
+	class list_spell : public spell
+	{
+	public:
+		list_spell() = default;
+		list_spell(const vl::Var& data) : spell(data) {};
+		list_spell(const vl::VarPtr& data) : spell(data) {};
+		list_spell(const cppgen::Spell& data) : spell(data) {};
+
+		enum retcode : int
+		{
+			OK,
+			ALREADY_EXISTS,
+			DATA_ERROR,
+			OPTION_MISSED,
+			STORE_ERROR,
+			COUNT
+		};
+		// TODO: return struct with message and code together value from cast()
+		int cast(const option_list& args, spl::context& ctx) override;
+
+	private:
+		using initializer = spell::initializer<list_spell>;
+		static const initializer m_initializer;
+	};
+}
+
---
+++ Spellbook/src/spells/list_spell.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vl.h>
+#include <utils/Log.h>
+#include "spells/list_spell.h"
+#include "generated/content.h"
+#include "generated/types.h"
+#include "spell_context.h"
+
+LOG_PREFIX("[list_spell]: ");
+LOG_POSTFIX("\n");
+
+#define MSG(m) std::cout << m << '\n'
+
+namespace spl
+{
+	// Static spell registrator
+	const list_spell::initializer list_spell::m_initializer("list");
+	int list_spell::cast(const option_list& args, spl::context& ctx)
+	{
+		auto spells = ctx.get_content_data().get_spellbook().get_spells();
+		auto& list = spells.get_list();
+		for (int i = 0; i < list.Size(); i++)
+			MSG(list.At(i).AsString().Val());
+		return retcode::OK;
+	}
+
+}
