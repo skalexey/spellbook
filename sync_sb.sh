7587bf8faca78e313977fe7b92b20679a6b0211a
skalexey <skorohodov-ad@narod.ru>
2022-07-20 17:04:24 +0300

    sync_sb.sh script for sync spell added

---
+++ sync_sb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source os.sh
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if is_mac; then
+	temp_dir="/var/folders/x3/s5vc1vgs4132nd81mblxzhc40000gn/T"
+else
+	temp_dir="${HOME}/AppData/Local/Temp"
+fi
+
+cp "$temp_dir/spellbook.json" "$THIS_DIR/resources"
