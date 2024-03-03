22522f771361a1d68d917c73d7b9d8646b5b0c7b
skalexey <skorohodov-ad@narod.ru>
2024-03-03 18:36:29 +0100

    ordered_map update.

--- Spellbook/include/spell_fwd.h
+++ Spellbook/include/spell_fwd.h
@@ -15,7 +15,7 @@ namespace spl
 	using spell_expression_ptr = std::shared_ptr<spell_expression>;
 	class context;
 	class parser;
-	using option_list = utils::ordered_map<std::string, cppgen::Option>;
+	using option_list = utils::ordered_hashmap<std::string, cppgen::Option>;
 	using args_list = std::vector<std::string>;
 	using context_func_t = std::function<void(spl::context&)>;
 }
