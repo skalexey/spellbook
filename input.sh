a729e385ea49e84f8c98a8e7cee88dc35bffe018
Alex <biggun@writeme.com>
2024-12-23 17:18:09 -0600

    Scripts auto update

--- input.sh
+++ input.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+function request_input() {
+	read -p "$@" input
+	[ $? -ne 0 ] && return 1
+	echo $input
+}
+
+function request_input_silent() {
+	read -s -p "$@" input
+	[ $? -ne 0 ] && return 1
+	echo $input
+}
+
 function ask_user() {
 	[ -z "$1" ] && echo "[ask_user]: No question provided" && return 1
 
 	local res=false
 
 	while true; do
-		read -p "$(echo $@) (y/n) " yn
-		case $yn in 
+		local answer=$(request_input "$@ (y/n): ")
+		case $answer in
 			[yY] ) res=true
 				break;;
 			[nN] ) res=false
