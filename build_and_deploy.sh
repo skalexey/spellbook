bf7e335f0fa7fab3aebbeddd2a0760c6d9947082
Alex <biggun@writeme.com>
2024-12-27 11:49:58 -0600

    Deploy scripts update

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,12 +2,27 @@
 
 function job()
 {
-	local tmp="${PWD}"
+	local current_dir=${PWD}
+	echo "current_dir: '$current_dir'"
 	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	cd "$THIS_DIR"
-	./build.sh . -rcb release
-	./deploy.sh
-	cd "$tmp"
+	source log.sh
+	local log_prefix="[build_and_deploy.sh]: "
+	local configuration="Release"
+	# iterate arguments case-insensitive
+	for arg in "$@"; do
+		arg=$(echo "$arg" | tr '[:upper:]' '[:lower:]')
+		if [ "$arg" == "debug" ]; then
+			log_info "debug argument passed"
+			configuration="Debug"
+		fi
+	done
+	log_warning "Build and deploy '$configuration' configuration"
+	source build.sh "$THIS_DIR" $configuration  # Source to apply environment variables in the case
+	[ $? -ne 0 ] && log_error " --- Errors during building" && exit 1
+	source $THIS_DIR/deploy.sh $@  # Source to apply environment variables in the case
+	[ $? -ne 0 ] && log_error " --- Errors during deploying" && exit 1
+	log_success " --- All done"
 }
 
 job $@
